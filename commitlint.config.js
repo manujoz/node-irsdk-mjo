1375975def2a71b1c11236a3294ccac74f9c0f93
Manu Overa <manu.overa@gmail.com>
2024-03-09 20:30:09 +0100

    feat(IRSDKDEV-4): add husky configuration
    feat(IRSDKDEV-4): add husky configuration

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [2, "always", ["build", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "dev"]],
+    },
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\w*)\(([\w-]*)\)!?:\s(add|fix|update|test|change|remove|panic|develop)\s([a-z0-9\s-#@.-_]*)$/,
+            headerCorrespondence: ["type", "ticket", "subjectprefix", "subject"],
+        },
+    },
+};
